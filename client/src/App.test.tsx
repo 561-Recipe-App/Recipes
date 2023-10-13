28111135be6e6edbecd1b8a86b2fb91463da5364
Hayden Thai <thaihayden@gmail.com>
2023-10-13 04:48:12 -0700

    fixed failing test

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,8 +1,14 @@
+import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(
+    <Router>
+      <App />
+    </Router>,
+  );
+  const linkElement = screen.getByText(/Recipe-App/i);
   expect(linkElement).toBeInTheDocument();
 });
